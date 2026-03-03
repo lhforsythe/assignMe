7d2fa70c848543c9b08c6ad63dbad1d2aa1bd8b4
lhforsythe <loganhenryslaman@gmail.com>
2026-03-03 16:34:58 -0500

    hide keys

--- djangoproject/index/views.py
+++ djangoproject/index/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import HttpResponse, render
 
 
 def view(request):  # defines html within view.
-    return HttpResponse("<h1>mr. squidward, IM GONNA KICK YOU FUDGING A..")
+    return HttpResponse("test")
