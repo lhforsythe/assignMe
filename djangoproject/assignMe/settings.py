6d790d4ca87d9e84230b9b805b9232153771dadd
lhforsythe <loganhenryslaman@gmail.com>
2026-03-03 20:20:09 -0500

    Update Docker dependencies, add allauth package, and integrate mySQL database with base django app

--- djangoproject/assignMe/settings.py
+++ djangoproject/assignMe/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "assignMe.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "user_data",
+        "USER": "admin",
+        "PASSWORD": "Snowbell1!",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
