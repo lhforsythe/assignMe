4673d59584b98ad7a6667b0bb446f70ec18ed92f
lhforsythe <loganhenryslaman@gmail.com>
2026-04-04 23:46:00 -0400

    Added ability to click each entry in search, which opens up a mini view of the assignment.

---
+++ djangoproject/main/templatetags/retrieveAssignment.py
@@ -0,0 +1,8 @@
+from ..models import Assignments
+
+register = template.Library()
+
+@register.filter
+def retrieveAssignment(reqKey):
+    assignment = Assignments.objects.get(key=reqKey)
+    return assignment
