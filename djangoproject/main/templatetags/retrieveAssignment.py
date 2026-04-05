4b26e65ef46e190e7e40fbf5a369b40280e97ac4
lhforsythe <loganhenryslaman@gmail.com>
2026-04-05 01:39:32 -0400

    another bug fix

--- djangoproject/main/templatetags/retrieveAssignment.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from ..models import Assignments
-
-register = template.Library()
-
-@register.filter
-def retrieveAssignment(reqKey):
-    assignment = Assignments.objects.get(key=reqKey)
-    return assignment
